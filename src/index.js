74d8dd9d599cb3b78da8d3fe04beb5141c9ad1be
sneh258 <snehalall.sneha@gmail.com>
2022-12-07 20:43:02 +0530

    website created

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Cards from './Cards';
+import Sdata from './Sdata';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+function ncard(val){
+    return(<Cards
+        key={val.id}
+        imgsrc={val.imgsrc}
+        title={val.title}
+        name={val.name}
+        link={val.link}
+        />
 );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+
+<>
+    <h1 className='heading_style'>List of top 5 Netflix Series</h1>
+  
+    
+     {Sdata.map(ncard)}
+
+
+
+    </>
+
+  , document.getElementById('root')
+);
