dd241129ca13b30b1d985956b6ade0dbb481ffac
sneh258 <snehalall.sneha@gmail.com>
2022-12-07 20:43:36 +0530

    website created

---
+++ src/Sdata.jsx
@@ -0,0 +1,47 @@
+const Sdata = [
+
+    {
+        id:1,
+    imgsrc:"https://flxt.tmsimg.com/assets/p17580186_b_v13_ab.jpg",
+    title:"A Netflix Original Series",
+    name:"The Witcher",
+    link:'https://www.netflix.com/in/title/80189685'
+
+    },
+
+    {id:2,
+        imgsrc:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcMvzTK3P4utQeyTN-LXqnSI0RVoqvqxT50J6jbX61_vBydTUS",
+     title:"A Netflix Original Series",
+     name:"Wednesday Addams",
+     link:'https://www.netflix.com/in/title/81231974'
+
+    },
+    { id:3,
+        imgsrc:"https://occ.a.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABV5lteI3eHet1nPBQKC_uEChESjqQfYpRwMWLR0wULt52odnxQtG69JNFDj9N4maZWJWDFZyi2sc1YzZ8mKE4Ajmt7Btva1CnWagGbYXVMp3t4OETM4LwiD70dHu-qWUbBAesQ.jpg?r=c68",
+     title:"A Netflix Original Series",
+     name:"Stranger Things",
+     link:'https://www.netflix.com/in/title/80057281'
+
+    },
+
+    { id:4,
+        imgsrc:"https://occ-0-33-32.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABUr6-gDdvQzjLXko1nnFGg2OcNRE5ZzRYjKlDbqMTDMcZPl_vjLkmsryR-2bH4yabSKW_rEMOaSV9B_Jaihls_RDRrspsp-sJro4cSFoVVL5Il-QOO0qUGv-WYHy8HCiEr-Ktw.jpg?r=4aa",
+     title:"A Netflix Original Series",
+     name:"The Mindhunter",
+     link:'https://www.netflix.com/in/title/80114855'
+
+
+    },
+
+    { id:5,
+        imgsrc:"https://occ-0-34-358.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABXrfn3Ap2ikHWYm07Of68ZP9cowjxHoDhgO5sP0izw2RPt99Lm0R4sfgGMvrKX3NLtG4nmqsjCEzxcQrZlsrr-OJ7W0c5yopuL82E3vH0dhsmRHmtZk0yM2oCCzW0lMW_ER_zw.jpg?r=f89",
+        title:"A Netflix Original Series",
+        name:"Sacred Games",
+        link:'https://www.netflix.com/in/title/80115328?source=35'
+
+    }
+
+
+]
+
+export default Sdata;
